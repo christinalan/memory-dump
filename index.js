c46801f134e6ddbe3fd8c275352fe1b2f70cfaa7
Christina Lan <christinalan@Christinas-MBP.home>
2020-10-21 11:27:43 -0400

    trash button will delete docs, but not from page

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ let app = express();
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-//DB code
+//DB code for good storage
 let Datastore = require('nedb');
-let db = new Datastore('memory.db');
+let db = new Datastore('goodMemory.db');
 db.loadDatabase();
+
+let dbBad = new Datastore('badMemory.db');
+dbBad.loadDatabase();
 
 let today = new Date();
 let newDate = today.toDateString();
 
-let goodMemories = [];
-let badMemories = [];
+// let goodMemories = [];
+// let badMemories = [];
 // app.get('/', (req, res) => {
 //     res.send('this is the main page');
 // })
@@ -39,11 +42,12 @@ app.post('/goodMemories', (req, res) => {
 //add route to get all logged good memory info
 app.get('/getGoodMemories', (req, res) => {
 
-    db.find({"memory" : goodMemory}, (err, docs)=> {
+    db.find({}, (err, docs)=> {
         if(err) {
             res.json({task:"task failed"})
         } else {
-            let obj = {data: goodMemories};
+            console.log(docs)
+            let obj = {data: docs};
             res.json(obj);
         }
     })
@@ -60,7 +64,7 @@ app.post('/badMemories', (req, res) => {
     }
 
     //insert memory into the memory database
-    db.insert(obj2, (err, newDocs) => {
+    dbBad.insert(obj2, (err, newDocs) => {
         if(err) {
             res.json({task:"task failed"})
         } else {
@@ -72,12 +76,30 @@ app.post('/badMemories', (req, res) => {
 
 //add route to store all logged bad memories
 app.get('/getBadMemories', (req, res) => {
-    db.find({"memory" : badMemory}, (err, docs)=> {
+    dbBad.find({}, (err, docs)=> {
         if(err) {
             res.json({task:"task failed"})
         } else {
-            let obj = {data: badMemories};
-            res.json(obj);
+            console.log(docs)
+            let obj2 = {data: docs};
+            res.json(obj2);
+        }
+    })
+})
+
+//routes for trashing bad memories
+app.post('/trashBadMem', (req, res) => {
+    console.log(req.body);
+})
+
+app.get('/trashedBadMem', (req, res) => {
+    dbBad.remove({}, (err, numRemoved)=> {
+        if(err) {
+            res.json({task:"task failed"})
+        } else {
+            console.log(numRemoved)
+            let obj2 = {data: numRemoved};
+            res.json(obj2);
         }
     })
 })
--- public/app.js
+++ public/app.js
@@ -1,3 +1,7 @@
+let trashClicked = false;
+let string;
+let element;
+
 window.addEventListener('load', () => {
     //posts the good memories from the good memory button
     document.getElementById('button-good').addEventListener('click', () => {
@@ -22,8 +26,9 @@ window.addEventListener('load', () => {
         }) 
     })
 
+    // fetch info from good memory database 
     document.getElementById('get-happy-tracker').addEventListener('click', () => {
-        fetch('getGoodMemories')
+        fetch('/getGoodMemories')
         .then(resp => resp.json())
         .then(data => {
             document.getElementById('happy-info').innerHTML = '';
@@ -46,7 +51,6 @@ window.addEventListener('load', () => {
         let jsonData = JSON.stringify(obj2);
 
         //make a fetch request of type POST so that we can send input to the server
-
         fetch('/badMemories', {
             method: 'POST',
             headers: {
@@ -56,23 +60,53 @@ window.addEventListener('load', () => {
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            console.log(data); //shows that the server has successfully inserted new memory
         }) 
+
+        //fetch request to send input to the trash button in the server
+        fetch('/trashBadMem', {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: jsonData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
     })
 
     document.getElementById('get-bad-tracker').addEventListener('click', () => {
-        fetch('getBadMemories')
+        fetch('/getBadMemories')
         .then(resp => resp.json())
         .then(data => {
             document.getElementById('bad-info').innerHTML = '';
             console.log(data.data);
-            for (let i=0; i< data.data.length; i++) {
-                let string = data.data[i].date + " : " + data.data[i].memory;
-                let element = document.createElement('p');
-                element.innerHTML = string;
-                document.getElementById('bad-info').appendChild(element);
-            }
+            if(trashClicked == true) {
+                document.getElementById('bad-info').innerHTML = '';
+            } else {
+                for (let i=0; i< data.data.length; i++) {
+                    string = data.data[i].date + " : " + data.data[i].memory;
+                    element = document.createElement('p');
+                    element.innerHTML = string;
+                    document.getElementById('bad-info').appendChild(element);
+                }
+            } trashClicked = false;
+       
         })
     })
+
+    document.getElementById('trash-image').addEventListener('click', () => {
+        fetch('/trashedBadMem')
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data.data);
+            // document.getElementById('bad-info').innerHTML = '';
+        })
+      trashClicked = true;
+      console.log(trashClicked);
+    })
+    
 })
 
